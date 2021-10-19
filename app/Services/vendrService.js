ce7cfeb7395a79810378fdf28f46f05e69e86e72
Don <appleonpoint@gmail.com>
2021-10-19 16:06:47 -0600

    Fixed issue with purchasability

--- app/Services/vendrService.js
+++ app/Services/vendrService.js
@@ -14,25 +14,25 @@ class VendrService {
   pch1() {
     if (ProxyState.Money >= 2.0) {
       ProxyState.Money -= 2.0
+      alert("You Bought Some Coffee")
+      console.log("You Bought Some Coffee")
     }
-    alert("You Bought Some Coffee")
-    console.log("You Bought Some Coffee")
   }
 
   pch2() {
     if (ProxyState.Money >= 4.0) {
       ProxyState.Money -= 4.0
+      alert('You Bought Some Cheetos')
+      console.log("You Bought Some Cheetos")
     }
-    alert('You Bought Some Cheetos')
-    console.log("You Bought Some Cheetos")
   }
 
   pch3() {
     if (ProxyState.Money >= 6.0) {
       ProxyState.Money -= 6.0
+      alert("You Bought Some Chicken (Gross, don't eat that!)")
+      console.log("You Bought Some Chicken")
     }
-    alert("You Bought Some Chicken (Gross, don't eat that!)")
-    console.log("You Bought Some Chicken")
   }
 }
 export const vendrservice = new VendrService()
